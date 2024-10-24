bbe6c2a5a1ec29605306e42d88ea01cac10cb766
larinhax1 <lara.f.silva9@aluno.senai.br>
2024-10-24 14:01:04 -0300

    criação login

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const form = document.getElementById ('form');
+  const email_ input = document.getElementById(' email-input');
+
+const password_input = document.getElementById ('password-input');
+ const error_message = document getElementById ('error-message');
+v form. addEventListener ('submit', (e) =>
+e. preventDefault);
+alert (email_input.value);
+error_message. innerText = password_input. value;
+5) ;
